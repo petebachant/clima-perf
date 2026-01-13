e346ea6b490b5757812e9f31b999bf833c8a6b36
Pete Bachant <petebachant@gmail.com>
2026-01-12 17:59:32 -0800

    Add sbatch config

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# TODO: module purge and load
-# TODO: set env vars? Maybe move to calkit.yaml?
+# Load modules
+module purge
+module load climacommon/2025_05_15
+
+# Set environment variable for GPU usage
+export CLIMACOMMS_DEVICE=CUDA
+
+# Set environmental variable for julia to not use global packages for
+# reproducibility
+export JULIA_LOAD_PATH=@:@stdlib
 
 calkit xenv -n main -- python scripts/run.py --date "$1"
